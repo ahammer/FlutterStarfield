ff41a11600ed846993ff47fa1290cd90d7862c87
Adam Hammer <adamhammer2@gmail.com>
2019-02-25 19:22:57 -0800

    Redux for setting the values

--- lib/widgets/MainScreen/MainScreenBody.dart
+++ lib/widgets/MainScreen/MainScreenBody.dart
@@ -3,10 +3,9 @@ import 'package:flutter_mysassa/starfield/StarField.dart';
 import 'package:flutter_mysassa/starfield/StarfieldPainter.dart';
 import 'package:flutter_mysassa/widgets/MainScreen/MainScreenBackground.dart';
 
-Widget getBody(StarField starField, double numStars) =>  Stack(
-    children: <Widget>[
+Widget getBody(StarField starField, double numStars) =>
+    Stack(children: <Widget>[
       getBackground(),
       CustomPaint(
-          painter: StarFieldPainter(starField, numStars),
-          size: Size.infinite),
+          painter: StarFieldPainter(starField, numStars), size: Size.infinite),
     ]);
--- lib/widgets/MainScreen/MainScreenFooter.dart
+++ lib/widgets/MainScreen/MainScreenFooter.dart
@@ -4,22 +4,24 @@ import 'package:flutter_mysassa/widgets/MainScreen/MainScreenRedux.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-Widget getFooter() => StoreConnector<MainScreenReduxState, Store<MainScreenReduxState>>(
-converter: (store) => store,
-builder: (context, viewModel) => Row(
-  children: <Widget>[
-    Flexible(
-        flex: 1,
-        child: Container(
-            height: 40,
-            color: Colors.transparent,
-            child: Slider(
-                value: viewModel.state.stars,
-                min: 0,
-                max: 8000,
-                onChanged: (progress){ viewModel.dispatch(ChangeStarCount(progress)); }
-))),
-    Text(viewModel.state.stars.floor().toString())
-  ],
-  mainAxisSize: MainAxisSize.max,
-));
+Widget getFooter() =>
+    StoreConnector<MainScreenReduxState, Store<MainScreenReduxState>>(
+        converter: (store) => store,
+        builder: (context, viewModel) => Row(
+              children: <Widget>[
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                        height: 40,
+                        color: Colors.transparent,
+                        child: Slider(
+                            value: viewModel.state.stars,
+                            min: 0,
+                            max: 8000,
+                            onChanged: (progress) {
+                              viewModel.dispatch(ChangeStarCount(progress));
+                            }))),
+                Text(viewModel.state.stars.floor().toString())
+              ],
+              mainAxisSize: MainAxisSize.max,
+            ));
