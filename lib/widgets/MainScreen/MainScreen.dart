d853e039a8778f073846838c88734eadc03ce877
Adam Hammer <adamhammer2@gmail.com>
2019-02-25 21:39:08 -0800

    Redux now working for the Star Display

--- lib/widgets/MainScreen/MainScreen.dart
+++ lib/widgets/MainScreen/MainScreen.dart
@@ -1,10 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_mysassa/starfield/StarField.dart';
 import 'package:flutter_mysassa/widgets/MainScreen/MainScreenBody.dart';
 import 'package:flutter_mysassa/widgets/MainScreen/MainScreenFooter.dart';
-import 'package:flutter_mysassa/widgets/MainScreen/MainScreenState.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({Key key, this.title}) : super(key: key);
--- lib/widgets/MainScreen/MainScreenInteractions.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_mysassa/widgets/MainScreen/MainScreen.dart';
-
-abstract class MainScreenInteractions {
-  void changeSlideValue(double value);
-}
